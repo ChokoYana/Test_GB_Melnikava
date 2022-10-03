9bd08c5c1c874d76077fc5898607091145424b06
Yana <yana.melnik2022@mail@mail.ru>
2022-10-03 13:52:04 +0300

    Added method CreateStringArray

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу, которая из имеющегося массива строк  формирует массив строк, длина которых меньше или равна 3.
+// 
+
+Console.WriteLine("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] CreateStringArray ()
+{
+    string[] array = new string[size];
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i + 1} element: ")
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
