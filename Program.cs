7fc9bd5ff04774332e355d6a58a1fb9f9c3b459d
Yana <yana.melnik2022@mail@mail.ru>
2022-10-03 14:30:15 +0300

    Added method LenghtOfArray

--- Program.cs
+++ Program.cs
@@ -3,13 +3,14 @@
 
 Console.WriteLine("Input size of array: ");
 int size = Convert.ToInt32(Console.ReadLine());
+const int line = 4;
 
 string[] CreateStringArray ()
 {
     string[] array = new string[size];
     for(int i = 0; i < size; i++)
     {
-        Console.Write($"Input {i + 1} element: ")
+        Console.Write($"Input {i + 1} element: ");
         array[i] = Console.ReadLine();
     }
     return array;
@@ -20,4 +21,20 @@ void ShowStringArray(string[] array)
     for(int i = 0; i < size; i++) Console.Write($"{array[i]}  ");
 }
 
-ShowStringArray(CreateStringArray());
+string[] LenghtOfArray(string[] array)
+{
+    string[] newArray = new string[array.Length];
+    for(int i = 0; i < size; i++)
+    {
+        if(array[i].Length < line)
+        {
+            newArray[i] = array[i];
+        }
+    }
+    return newArray;
+}
+
+string[] myArray = CreateStringArray();
+ShowStringArray(myArray);
+Console.WriteLine();
+ShowStringArray(LenghtOfArray(myArray));
